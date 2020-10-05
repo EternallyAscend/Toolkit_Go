bcf22a649a6d99fe17c20476add3b0bd72a83c4b
EternallyAscend <Eternally-Ascend@Outlook.com>
2020-10-05 13:52:14 +0800

    Finish AES tools.

--- AES/AES.go
+++ AES/AES.go
@@ -15,15 +15,19 @@ func TestAES() {
 	originData := []byte("Tom")
 	fmt.Println("Origin Data is:")
 	fmt.Println("Tom")
-	en := EncryptAES(originData, key)
+	en := encryptAES(originData, key)
 	fmt.Println("The result is:")
 	fmt.Println(en)
-	de := DecryptAES(en, key)
+	de := decryptAES(en, key)
 	fmt.Println("The result is:")
 	fmt.Println(string(de))
 }
 
-func DecryptAES(cipherText, key []byte) []byte {
+func generateRandomKeyOfAES() []byte {
+	return []byte(uuid.Must(uuid.NewV4()).String()[:16])
+}
+
+func decryptAES(cipherText, key []byte) []byte {
 	block, _ := aes.NewCipher(key)
 	blockSize := block.BlockSize()
 	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
@@ -39,7 +43,7 @@ func PKCS7UnPadding(originData []byte) []byte {
 	return originData[:length-position]
 }
 
-func EncryptAES(originData, key []byte) []byte {
+func encryptAES(originData, key []byte) []byte {
 	block, _ := aes.NewCipher(key)
 	originData = PKCS7Padding(originData, block.BlockSize())
 	blockMode := cipher.NewCBCEncrypter(block, key[:block.BlockSize()])
